39ec5fb711645256ebfdd55b8d269b75e2edc983
imperfect-12 <b.prashant@iitg.ac.in>
2025-06-03 16:54:57 +0530

    The basic game completes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import { useState } from "react";
 
-function Square({ value }) {
-
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+
   const [squares, setSquares] = useState(Array(9).fill(null));
+
+  const winner = calculateWinner(squares);
+  const allFilled = checkAll(squares);
+  let status;
+  if (winner) {
+    status = "Winner : " + winner;
+  } else if (allFilled) {
+    status = "Game ends !! No-one wins"
+  } else {
+    status = "Next player : " + (xIsNext ? "X" : "O");
+  }
+
   function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
     const nextSquares = squares.slice();
-    nextSquares[i] = 'X';
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
     setSquares(nextSquares);
+    setXIsNext(!xIsNext);
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -36,4 +51,42 @@ export default function Board() {
       </div>
     </>
   );
+}
+
+function Square({ value, onSquareClick }) {
+  return (
+    <button className="square" onClick={onSquareClick}>
+      {value}
+    </button>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [1, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+function checkAll(squares) {
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i]) {
+      return false;
+    }
+  }
+  return true;
 }
